233f51171b8252df19b16c35e272328a0339e663
Jackson Yeager <jacksonyeager@gmail.com>
2020-09-05 03:53:43 -0400

    Updates

--- traveler/service-worker.js
+++ traveler/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/traveler/precache-manifest.cc836944e982109aaa76a144d78e6256.js"
+  "/traveler/precache-manifest.a02223c6dfab1066db239d9e10f0feee.js"
 );
 
 self.addEventListener('message', (event) => {
