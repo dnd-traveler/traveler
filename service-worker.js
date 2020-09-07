90ee10d9540b81da8975738aa7f9df56bef2599c
Jackson Yeager <jacksonyeager@gmail.com>
2020-09-07 04:17:07 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75627ccddd5a678ad06487ef5352a978.js"
+  "/precache-manifest.70f78e15029d46c6539e2f79c05ea292.js"
 );
 
 self.addEventListener('message', (event) => {
